6c273184ca92606acd27f70866be2cfa083a2829
K-GRAM\kohmi <kohminji10@gmail.com>
2022-04-13 22:01:18 +0900

    compelete 2.내 진료 정보 확인

--- ConsoleProject/src/com/project/dentist/DiagnosisInfo.java
+++ ConsoleProject/src/com/project/dentist/DiagnosisInfo.java
@@ -8,11 +8,6 @@ public class DiagnosisInfo {
 	private String PatientNum;
 	private String DoctorNum;
 
-	private Calendar date;  //TODO date + time 하나로? **
-	private String SymptomNum //증상번호
-	private String treatmentNum //시술번호
-	private String resevationNum //예약번호
-	
 	private Calendar dateTime;  //TODO date + time 하나로 **
 	private String SymptomNum; //증상번호
 	private String treatmentNum; //시술번호
